c7c206a72a419d7b9c491771d469350e3c1d25bc
LeeNaKyung <lnag123@naver.com>
2024-09-30 06:25:57 +0000

    5 line/Sort in long order

--- chap3/hw1/main.c
+++ chap3/hw1/main.c
@@ -1,25 +1,39 @@
 #include <stdio.h>
 #include <string.h>
-#include "copy.h"
-char line[MAXLINE]; 
-char longest[MAXLINE]; 
-void copy(char from[], char to[]);
+#define MAXLINE 100
+#define MAXLINES 5
 
-int main()
-{
-   int len, max = 0;
+char lines[MAXLINES][MAXLINE];
 
-   while (fgets(line,MAXLINE,stdin) != NULL) {
-      len = strlen(line);
-      if (len > max) {
-         max = len;
-         copy(line, longest);
-      }
-   }
-   
-   if (max > 0) ´
-   printf("%s", longest);
+int main() {
+    int len, max = 0;
 
-   return 0;
+    for (int i = 0; i < MAXLINES; i++) {
+        if (fgets(lines[i], MAXLINE, stdin) == NULL) {
+            break;
+        }
+        len = strlen(lines[i]);
+        if (len > max) {
+            max = len;
+        }
+    }
+
+    for (int i = 0; i < MAXLINES - 1; i++) {
+        for (int j = 0; j < MAXLINES - i - 1; j++) {
+            if (strlen(lines[j]) < strlen(lines[j + 1])) {
+                char temp[MAXLINE];
+                strcpy(temp, lines[j]);
+                strcpy(lines[j], lines[j + 1]);
+                strcpy(lines[j + 1], temp);
+            }
+        }
+    }
+
+    for (int i = 0; i < MAXLINES; i++) {
+        if (strlen(lines[i]) > 0) {
+            printf("%s", lines[i]);
+        }
+    }
+    return 0;
 }
 
